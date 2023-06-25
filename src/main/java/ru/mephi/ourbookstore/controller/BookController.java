a7e89c52fad75186ba9ac20d8cfa89753b2c26bc
alekseiiagnenkov <alekseiiagnenkov@gmail.com>
2023-06-25 14:45:34 +0300

    fix

--- src/main/java/ru/mephi/ourbookstore/controller/BookController.java
+++ src/main/java/ru/mephi/ourbookstore/controller/BookController.java
@@ -1,6 +1,7 @@
 package ru.mephi.ourbookstore.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.mephi.ourbookstore.domain.BookDto;
@@ -9,8 +10,8 @@ import ru.mephi.ourbookstore.domain.BookDto;
 @RequestMapping("/books")
 public class BookController {
 
-    @GetMapping
-    BookDto getBook() {
+    @GetMapping("/{bookId}")
+    public BookDto getBook(@PathVariable String bookId) {
         return new BookDto("Мертвые души", "Гоголь");
     }
 }
