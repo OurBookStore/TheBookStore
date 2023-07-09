482b55766a42ba66f988cf307d073966567c497e
GerasimychevDM <denis.gerasimychev@mail.ru>
2023-07-09 12:58:47 +0300

    was added fix swagger configuration

--- src/main/java/ru/mephi/ourbookstore/config/swagger/SwaggerConfig.java
+++ src/main/java/ru/mephi/ourbookstore/config/swagger/SwaggerConfig.java
@@ -1,20 +1,22 @@
 package ru.mephi.ourbookstore.config.swagger;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.ApiInfo;
 
 @Configuration
 public class SwaggerConfig {
+
     private static final String SWAGGER_TITLE = "Book store";
     private static final String SWAGGER_DESC = "This are some implementation book store";
+    private static final String SWAGGER_VERSION = "1.0";
 
     @Bean
-    public ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
+    public OpenAPI apiInfo() {
+        return new OpenAPI().info(new Info()
                 .title(SWAGGER_TITLE)
                 .description(SWAGGER_DESC)
-                .build();
+                .version(SWAGGER_VERSION));
     }
 }
