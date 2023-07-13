6e299df14ad818939135e5a3dcbc32a59301f537
alekseiiagnenkov <alekseiiagnenkov@gmail.com>
2023-07-13 23:48:14 +0300

    add new dtos

---
+++ src/main/java/ru/mephi/ourbookstore/domain/dto/book/BookUpdateDto.java
@@ -0,0 +1,20 @@
+package ru.mephi.ourbookstore.domain.dto.book;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * @author Aleksei Iagnenkov (alekseiiagn)
+ */
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class BookUpdateDto {
+
+    Long id;
+    String name;
+    double price;
+    int count;
+}
--- src/main/java/ru/mephi/ourbookstore/mapper/book/BookDtoMapper.java
+++ src/main/java/ru/mephi/ourbookstore/mapper/book/BookDtoMapper.java
@@ -2,7 +2,9 @@ package ru.mephi.ourbookstore.mapper.book;
 
 import org.mapstruct.Mapper;
 import ru.mephi.ourbookstore.domain.dto.book.Book;
+import ru.mephi.ourbookstore.domain.dto.book.BookCreateDto;
 import ru.mephi.ourbookstore.domain.dto.book.BookDto;
+import ru.mephi.ourbookstore.domain.dto.book.BookUpdateDto;
 
 /**
  * @author Aleksei Iagnenkov (alekseiiagn)
@@ -10,7 +12,9 @@ import ru.mephi.ourbookstore.domain.dto.book.BookDto;
 @Mapper(componentModel = "spring")
 public interface BookDtoMapper {
 
-    Book dtoToObject(BookDto model);
+    Book dtoToObject(BookCreateDto dto);
+
+    Book dtoToObject(BookUpdateDto dto);
 
     BookDto objectToDto(Book book);
 }
